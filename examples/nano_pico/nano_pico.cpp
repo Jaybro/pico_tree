a1a64b7d948c3d4840d567efe34fc5b23eced917
Jonathan Broere <j_broere@yahoo.com>
2020-09-08 22:53:56 +0200

    Changed nano_pico demo into a benchmark.

--- examples/nano_pico/nano_pico.cpp
+++ examples/nano_pico/nano_pico.cpp
@@ -1,124 +1,265 @@
+#include <benchmark/benchmark.h>
+
 #include <pico_point_set.hpp>
 #include <pico_tree/kd_tree.hpp>
 #include <scoped_timer.hpp>
 
+#include "format_bin.hpp"
 #include "nano_point_set.hpp"
 
-// *****************************************************************************
-// Not a benchmark, just a simple comparison.
-// *****************************************************************************
-
-using Index = int;
-
-void BuildTree() {
-  using PointX = Point2f;
+class KdTreeBenchmark : public benchmark::Fixture {
+ protected:
+  using Index = int;
+  using Scalar = double;
+  using PointX = Point3d;
   using NanoPointSetX = NanoPointSet<Index, PointX>;
   using PicoPointSetX = PicoPointSet<Index, PointX>;
 
-  int run_count = 4096;
-  int max_leaf_count = 1;
-  std::vector<PointX> random = GenerateRandomN<PointX>(1024, 100);
-  NanoPointSetX nano_points(random);
-  PicoPointSetX pico_points(random);
-
-  {
-    ScopedTimer t("tree gen nanoflann ct", run_count);
-    for (Index i = 0; i < run_count; ++i) {
-      NanoflannKdTree<NanoPointSetX> kt(
-          PointX::Dims,
-          nano_points,
-          nanoflann::KDTreeSingleIndexAdaptorParams(max_leaf_count));
-      kt.buildIndex();
-    }
+ public:
+  KdTreeBenchmark() {
+    // Here you may need to be patient. Depending on the size of the compiled
+    // binary.
+    pico_tree::ReadBin("./scans.bin", &points_);
   }
 
-  {
-    ScopedTimer t("tree gen nanoflann rt", run_count);
-    for (Index i = 0; i < run_count; ++i) {
-      NanoflannKdTreeRt<NanoPointSetX> kt(
-          PointX::Dims,
-          nano_points,
-          nanoflann::KDTreeSingleIndexAdaptorParams(max_leaf_count));
-      kt.buildIndex();
-    }
-  }
+ protected:
+  std::vector<PointX> points_;
+};
 
-  {
-    ScopedTimer t("tree gen pico_tree ct", run_count);
-    for (Index i = 0; i < run_count; ++i) {
-      KdTree<PicoPointSetX> rt(pico_points, max_leaf_count);
-    }
-  }
+// ****************************************************************************
+// Building the tree
+// ****************************************************************************
 
-  {
-    ScopedTimer t("tree gen pico_tree rt", run_count);
-    for (Index i = 0; i < run_count; ++i) {
-      KdTreeRt<PicoPointSetX> rt(pico_points, max_leaf_count);
-    }
-  }
-}
-
-void SearchKnn() {
-  using PointX = Point3f;
-  using Scalar = PointX::Scalar;
-  using NanoPointSetX = NanoPointSet<Index, PointX>;
-  using PicoPointSetX = PicoPointSet<Index, PointX>;
-
-  int run_count = 1024 * 1024;
-  int point_count = 1024 * 1024;
-  float area_size = 1000;
-
-  int max_leaf_count = 8;
-  std::vector<PointX> random = GenerateRandomN<PointX>(point_count, area_size);
-  NanoPointSetX nano_points(random);
-  PicoPointSetX pico_points(random);
-
-  constexpr int k = 10;
-
-  // nano flann
-  Index indices[k];
-  Scalar distances[k];
-  std::vector<std::pair<Index, Scalar>> nf;
-  nf.reserve(k);
-
-  auto p = GenerateRandomP<PointX>(area_size);
-  Scalar v = 7.5f;
-  Scalar radius = v * v;
-
-  {
-    NanoflannKdTree<NanoPointSetX> kt(
+BENCHMARK_DEFINE_F(KdTreeBenchmark, CtNanoBuildTree)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  NanoPointSetX nano_set(points_);
+  for (auto _ : state) {
+    NanoflannKdTree<NanoPointSetX> tree(
         PointX::Dims,
-        nano_points,
-        nanoflann::KDTreeSingleIndexAdaptorParams(max_leaf_count));
-    kt.buildIndex();
-
-    ScopedTimer t("tree nn_ nanoflann", run_count);
-    for (Index i = 0; i < run_count; ++i) {
-      kt.knnSearch(p.data, 1, indices, distances);
-      kt.knnSearch(p.data, k, indices, distances);
-      kt.radiusSearch(p.data, radius, nf, nanoflann::SearchParams{0, 0, false});
-    }
+        nano_set,
+        nanoflann::KDTreeSingleIndexAdaptorParams(max_leaf_size));
+    tree.buildIndex();
   }
+}
 
-  // pico tree
-  std::pair<Index, Scalar> nn;
-  std::vector<std::pair<Index, Scalar>> n;
-  n.reserve(k);
+BENCHMARK_DEFINE_F(KdTreeBenchmark, CtPicoBuildTree)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  PicoPointSetX pico_set(points_);
+  for (auto _ : state) {
+    KdTree<PicoPointSetX> tree(pico_set, max_leaf_size);
+  }
+}
 
-  {
-    KdTree<PicoPointSetX> rt(pico_points, max_leaf_count);
+BENCHMARK_DEFINE_F(KdTreeBenchmark, RtNanoBuildTree)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  NanoPointSetX nano_set(points_);
+  for (auto _ : state) {
+    NanoflannKdTreeRt<NanoPointSetX> tree(
+        PointX::Dims,
+        nano_set,
+        nanoflann::KDTreeSingleIndexAdaptorParams(max_leaf_size));
+    tree.buildIndex();
+  }
+}
 
-    ScopedTimer t("tree nn_ pico_tree", run_count);
-    for (Index i = 0; i < run_count; ++i) {
-      nn = rt.SearchNn(p);
-      rt.SearchKnn(p, k, &n);
-      rt.SearchRadius(p, radius, &n);
+BENCHMARK_DEFINE_F(KdTreeBenchmark, RtPicoBuildTree)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  PicoPointSetX pico_set(points_);
+  for (auto _ : state) {
+    KdTreeRt<PicoPointSetX> tree(pico_set, max_leaf_size);
+  }
+}
+
+// Argument 1: Maximum leaf size.
+BENCHMARK_REGISTER_F(KdTreeBenchmark, CtNanoBuildTree)
+    ->Unit(benchmark::kMillisecond)
+    ->Arg(1)
+    ->DenseRange(6, 14, 2);
+BENCHMARK_REGISTER_F(KdTreeBenchmark, CtPicoBuildTree)
+    ->Unit(benchmark::kMillisecond)
+    ->Arg(1)
+    ->DenseRange(6, 14, 2);
+
+BENCHMARK_REGISTER_F(KdTreeBenchmark, RtNanoBuildTree)
+    ->Unit(benchmark::kMillisecond)
+    ->Arg(1)
+    ->DenseRange(6, 14, 2);
+BENCHMARK_REGISTER_F(KdTreeBenchmark, RtPicoBuildTree)
+    ->Unit(benchmark::kMillisecond)
+    ->Arg(1)
+    ->DenseRange(6, 14, 2);
+
+// ****************************************************************************
+// Knn
+// ****************************************************************************
+
+BENCHMARK_DEFINE_F(KdTreeBenchmark, CtNanoKnn)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  int knn_count = state.range(1);
+  NanoPointSetX nano_set(points_);
+  NanoflannKdTree<NanoPointSetX> tree(
+      PointX::Dims,
+      nano_set,
+      nanoflann::KDTreeSingleIndexAdaptorParams(max_leaf_size));
+  tree.buildIndex();
+
+  for (auto _ : state) {
+    std::vector<Index> indices(knn_count);
+    std::vector<Scalar> distances(knn_count);
+    std::size_t sum = 0;
+    for (auto const& p : points_) {
+      benchmark::DoNotOptimize(
+          sum +=
+          tree.knnSearch(p.data, knn_count, indices.data(), distances.data()));
     }
   }
 }
 
-int main() {
-  BuildTree();
-  SearchKnn();
-  return 0;
+BENCHMARK_DEFINE_F(KdTreeBenchmark, CtPicoKnn)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  int knn_count = state.range(1);
+  PicoPointSetX pico_set(points_);
+  KdTree<PicoPointSetX> tree(pico_set, max_leaf_size);
+
+  for (auto _ : state) {
+    std::vector<std::pair<Index, Scalar>> results;
+    std::size_t sum = 0;
+    for (auto const& p : points_) {
+      tree.SearchKnn(p, knn_count, &results);
+      benchmark::DoNotOptimize(sum += results.size());
+    }
+  }
 }
+
+// Argument 1: Maximum leaf size.
+// Argument 2: K nearest neighbors.
+BENCHMARK_REGISTER_F(KdTreeBenchmark, CtNanoKnn)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({1, 1})
+    ->Args({6, 1})
+    ->Args({8, 1})
+    ->Args({10, 1})
+    ->Args({12, 1})
+    ->Args({14, 1})
+    ->Args({1, 4})
+    ->Args({6, 4})
+    ->Args({8, 4})
+    ->Args({10, 4})
+    ->Args({12, 4})
+    ->Args({14, 4})
+    ->Args({1, 8})
+    ->Args({6, 8})
+    ->Args({8, 8})
+    ->Args({10, 8})
+    ->Args({12, 8})
+    ->Args({14, 8})
+    ->Args({1, 12})
+    ->Args({6, 12})
+    ->Args({8, 12})
+    ->Args({10, 12})
+    ->Args({12, 12})
+    ->Args({14, 12});
+
+BENCHMARK_REGISTER_F(KdTreeBenchmark, CtPicoKnn)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({1, 1})
+    ->Args({6, 1})
+    ->Args({8, 1})
+    ->Args({10, 1})
+    ->Args({12, 1})
+    ->Args({14, 1})
+    ->Args({1, 4})
+    ->Args({6, 4})
+    ->Args({8, 4})
+    ->Args({10, 4})
+    ->Args({12, 4})
+    ->Args({14, 4})
+    ->Args({1, 8})
+    ->Args({6, 8})
+    ->Args({8, 8})
+    ->Args({10, 8})
+    ->Args({12, 8})
+    ->Args({14, 8})
+    ->Args({1, 12})
+    ->Args({6, 12})
+    ->Args({8, 12})
+    ->Args({10, 12})
+    ->Args({12, 12})
+    ->Args({14, 12});
+
+// ****************************************************************************
+// Radius
+// ****************************************************************************
+
+BENCHMARK_DEFINE_F(KdTreeBenchmark, CtNanoRadius)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  double radius = static_cast<double>(state.range(1)) / 4.0;
+  double squared = radius * radius;
+  NanoPointSetX nano_set(points_);
+  NanoflannKdTree<NanoPointSetX> tree(
+      PointX::Dims,
+      nano_set,
+      nanoflann::KDTreeSingleIndexAdaptorParams(max_leaf_size));
+  tree.buildIndex();
+
+  for (auto _ : state) {
+    std::vector<std::pair<Index, Scalar>> results;
+    std::size_t sum = 0;
+    for (auto const& p : points_) {
+      benchmark::DoNotOptimize(
+          sum += tree.radiusSearch(
+              p.data, squared, results, nanoflann::SearchParams{0, 0, false}));
+    }
+  }
+}
+
+BENCHMARK_DEFINE_F(KdTreeBenchmark, CtPicoRadius)(benchmark::State& state) {
+  int max_leaf_size = state.range(0);
+  double radius = static_cast<double>(state.range(1)) / 4.0;
+  double squared = radius * radius;
+  PicoPointSetX pico_set(points_);
+  KdTree<PicoPointSetX> tree(pico_set, max_leaf_size);
+
+  for (auto _ : state) {
+    std::vector<std::pair<Index, Scalar>> results;
+    std::size_t sum = 0;
+    for (auto const& p : points_) {
+      tree.SearchRadius(p, squared, &results);
+      benchmark::DoNotOptimize(sum += results.size());
+    }
+  }
+}
+
+// Argument 1: Maximum leaf size.
+// Argument 2: Search radius (divided by 4.0).
+BENCHMARK_REGISTER_F(KdTreeBenchmark, CtNanoRadius)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({1, 1})
+    ->Args({6, 1})
+    ->Args({8, 1})
+    ->Args({10, 1})
+    ->Args({12, 1})
+    ->Args({14, 1})
+    ->Args({1, 2})
+    ->Args({6, 2})
+    ->Args({8, 2})
+    ->Args({10, 2})
+    ->Args({12, 2})
+    ->Args({14, 2});
+
+BENCHMARK_REGISTER_F(KdTreeBenchmark, CtPicoRadius)
+    ->Unit(benchmark::kMillisecond)
+    ->Args({1, 1})
+    ->Args({6, 1})
+    ->Args({8, 1})
+    ->Args({10, 1})
+    ->Args({12, 1})
+    ->Args({14, 1})
+    ->Args({1, 2})
+    ->Args({6, 2})
+    ->Args({8, 2})
+    ->Args({10, 2})
+    ->Args({12, 2})
+    ->Args({14, 2});
+
+BENCHMARK_MAIN();
