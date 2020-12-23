3d1999ab2a4233cf25d1e3489cbcab85a42e6917
Jonathan Broere <j_broere@yahoo.com>
2020-12-23 01:36:59 +0100

    Removed ReplaceFrontHeap.

--- src/pico_tree/core.hpp
+++ src/pico_tree/core.hpp
@@ -21,67 +21,6 @@ static constexpr int kDynamicDim = -1;
 
 namespace internal {
 
-//! \brief Restores the heap property of the range defined by \p begin and \p
-//! end, assuming only the top element could have broken it.
-//! \details Worst case performs O(2 log n) comparisons and O(log n) copies.
-//! Performance will be better in practice because it is possible to "early
-//! out" as soon as the first node is encountered that adheres to the heap
-//! property.
-//! <p/>
-//! A function for replacing the top of the heap is not available using the
-//! C++ stl. It is possible to use something like std::make_heap() or
-//! range.push_back() followed by a std::pop_heap() to get the desired effect.
-//! However, these solutions are either forced to traverse the entire range or
-//! introduce overhead, which seem to make them quite a bit slower.
-//! <p/>
-//! It should be possible to use this function in combination with any of the
-//! C++ stl heap related functions, as the documention of those suggest that the
-//! heap is implemented as a binary heap.
-template <typename RandomAccessIterator, typename Compare>
-inline void ReplaceFrontHeap(
-    RandomAccessIterator begin, RandomAccessIterator end, Compare comp) {
-  auto const size = end - begin;
-
-  if (size < 2) {
-    return;
-  }
-
-  typename std::iterator_traits<RandomAccessIterator>::difference_type parent =
-      0;
-  auto front = std::move(begin[parent]);
-  auto const last_parent = (size - 2) / 2;
-  while (parent < last_parent) {
-    auto child = 2 * parent + 1;
-    if (comp(begin[child], begin[child + 1])) {
-      ++child;
-    }
-    if (!comp(front, begin[child])) {
-      begin[parent] = std::move(front);
-      return;
-    } else {
-      begin[parent] = std::move(begin[child]);
-      parent = child;
-    }
-  }
-  // Everything below (but for replacing the last child) is the same as
-  // inside the loop. The only difference is that for even sized vectors we
-  // can't compare with the second child of the last parent.
-  //
-  // Assuming doing the check once outside of the loop is better?
-  if (parent == last_parent) {
-    auto child = 2 * parent + 1;
-    if ((size & 1) == 1 && comp(begin[child], begin[child + 1])) {
-      ++child;
-    }
-    if (comp(front, begin[child])) {
-      begin[parent] = std::move(begin[child]);
-      parent = child;
-    }
-  }
-  // Last child gets replaced.
-  begin[parent] = std::move(front);
-}
-
 //! \brief Inserts \p item in O(n) time at the index for which \p comp
 //! first holds true. The sequence must be sorted and remains sorted after
 //! insertion. The last item in the sequence is "pushed out".
