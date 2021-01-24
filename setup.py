4898ce33db057e967b80f8298978482349c1c819
Jonathan Broere <j_broere@yahoo.com>
2021-01-25 00:21:38 +0100

    Version comment updated.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from skbuild import setup
 
 setup(name='pico_tree',
-      # The version of PycoTree follows that of PicoTree.
+      # The same as the CMake project version.
       version='0.6.0',
       description='PicoTree Python Bindings',
       author='Jonathan Broere',
-      # url='https://github.com/Jaybro/pico_tree',
+      url='https://github.com/Jaybro/pico_tree',
       license="MIT",
       packages=["pico_tree"],
       package_dir={"": "src/pyco_tree"},
