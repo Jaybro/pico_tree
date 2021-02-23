536a411da34d6cb391022fe61b92c7127856f399
Jonathan Broere <j_broere@yahoo.com>
2021-02-23 23:22:25 +0100

    Added the L2 metric.

--- src/pico_tree/pico_tree/metric.hpp
+++ src/pico_tree/pico_tree/metric.hpp
@@ -36,7 +36,50 @@ class L1 {
     return d;
   }
 
-  //! \brief Calculates the difference between two coordinates.
+  //! \brief Calculates the distance between two coordinates.
+  inline Scalar operator()(Scalar const x, Scalar const y) const {
+    return std::abs(x - y);
+  }
+
+  //! \brief Returns the absolute value of \p x.
+  inline Scalar operator()(Scalar const x) const { return std::abs(x); }
+
+ private:
+  int const dim_;
+};
+
+//! \brief L2 metric for measuring Euclidean distances between points.
+//! \details https://en.wikipedia.org/wiki/Euclidean_distance
+//! \see L1
+template <typename Scalar, int Dim>
+class L2 {
+ public:
+  //! \brief Creates an L2Squared given a spatial dimension.
+  inline explicit L2(int const dim) : dim_{dim} {}
+
+  //! \brief Calculates the distance between points \p p0 and \p p1.
+  //! \tparam P0 Point type.
+  //! \tparam P1 Point type.
+  //! \param p0 Point.
+  //! \param p1 Point.
+  template <typename P0, typename P1>
+  // The enable_if is not required but it forces implicit casts which are
+  // handled by operator()(Scalar, Scalar).
+  inline typename std::enable_if<
+      !std::is_fundamental<P0>::value && !std::is_fundamental<P1>::value,
+      Scalar>::type
+  operator()(P0 const& p0, P1 const& p1) const {
+    Scalar d{};
+
+    for (int i = 0; i < internal::Dimension<Dim>::Dim(dim_); ++i) {
+      Scalar const v = p0(i) - p1(i);
+      d += v * v;
+    }
+
+    return std::sqrt(d);
+  }
+
+  //! \brief Calculates the distance between two coordinates.
   inline Scalar operator()(Scalar const x, Scalar const y) const {
     return std::abs(x - y);
   }
@@ -51,6 +94,7 @@ class L1 {
 //! \brief The L2Squared semimetric measures squared Euclidean distances between
 //! points.
 //! \see L1
+//! \see L2
 template <typename Scalar, int Dim>
 class L2Squared {
  public:
@@ -79,7 +123,7 @@ class L2Squared {
     return d;
   }
 
-  //! \brief Calculates the difference between two coordinates.
+  //! \brief Calculates the distance between two coordinates.
   inline Scalar operator()(Scalar const x, Scalar const y) const {
     Scalar const d = x - y;
     return d * d;
