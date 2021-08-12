eacb99e18ea3db3c41377cb031282a0021809392
Jonathan Broere <j_broere@yahoo.com>
2021-08-12 22:11:42 +0200

    Removed various warnings for the Box unit tests.

--- test/pico_tree/box_test.cpp
+++ test/pico_tree/box_test.cpp
@@ -17,22 +17,24 @@ class BoxTest : public testing::Test {};
 template <typename Scalar_, int Dim_>
 class BoxTest<Box<Scalar_, Dim_>> : public testing::Test {
  public:
-  BoxTest() : box(kDynamicBoxDim) {}
+  BoxTest() : box_(Dim(Dim_)) {}
 
-  Box<Scalar_, Dim_> box;
+ protected:
+  Box<Scalar_, Dim_> box_;
 };
 
 template <typename Scalar_, int Dim_>
 class BoxTest<BoxMap<Scalar_, Dim_>> : public testing::Test {
  public:
   BoxTest()
-      : min(Dim(Dim_)),
-        max(Dim(Dim_)),
-        box(min.data(), max.data(), Dim(Dim_)) {}
+      : min_(Dim(Dim_)),
+        max_(Dim(Dim_)),
+        box_(min_.data(), max_.data(), Dim(Dim_)) {}
 
-  std::vector<Scalar_> min;
-  std::vector<Scalar_> max;
-  BoxMap<Scalar_, Dim_> box;
+ protected:
+  std::vector<Scalar_> min_;
+  std::vector<Scalar_> max_;
+  BoxMap<Scalar_, Dim_> box_;
 };
 
 using BoxTestTypes = testing::Types<
@@ -48,50 +50,50 @@ TYPED_TEST_SUITE(BoxTest, BoxTestTypes);
 TYPED_TEST(BoxTest, size) {
   int dim = TypeParam::Dim;
   if (dim != kDynamicDim) {
-    EXPECT_EQ(this->box.size(), dim);
+    EXPECT_EQ(this->box_.size(), dim);
   } else {
-    EXPECT_EQ(this->box.size(), kDynamicBoxDim);
+    EXPECT_EQ(this->box_.size(), kDynamicBoxDim);
   }
 }
 
 TYPED_TEST(BoxTest, Accessors) {
-  this->box.FillInverseMax();
-  for (std::size_t i = 0; i < this->box.size(); ++i) {
-    EXPECT_FLOAT_EQ(this->box.min(i), this->box.min()[i]);
-    EXPECT_FLOAT_EQ(this->box.max(i), this->box.max()[i]);
+  this->box_.FillInverseMax();
+  for (std::size_t i = 0; i < this->box_.size(); ++i) {
+    EXPECT_EQ(this->box_.min(i), this->box_.min()[i]);
+    EXPECT_EQ(this->box_.max(i), this->box_.max()[i]);
   }
 }
 
 TYPED_TEST(BoxTest, FillInverseMax) {
   using Scalar = typename TypeParam::ScalarType;
 
-  this->box.FillInverseMax();
-  for (std::size_t i = 0; i < this->box.size(); ++i) {
-    EXPECT_FLOAT_EQ(this->box.min(i), std::numeric_limits<Scalar>::max());
-    EXPECT_FLOAT_EQ(this->box.max(i), std::numeric_limits<Scalar>::lowest());
+  this->box_.FillInverseMax();
+  for (std::size_t i = 0; i < this->box_.size(); ++i) {
+    EXPECT_EQ(this->box_.min(i), std::numeric_limits<Scalar>::max());
+    EXPECT_EQ(this->box_.max(i), std::numeric_limits<Scalar>::lowest());
   }
 }
 
 TYPED_TEST(BoxTest, Fit) {
   using Scalar = typename TypeParam::ScalarType;
 
-  this->box.FillInverseMax();
+  this->box_.FillInverseMax();
   int dim = Dim(TypeParam::Dim);
 
   // Fit a point.
   std::vector<Scalar> p(static_cast<std::size_t>(dim), Scalar(0));
-  this->box.Fit(p.data());
-  for (std::size_t i = 0; i < this->box.size(); ++i) {
-    EXPECT_FLOAT_EQ(this->box.min(i), Scalar(0));
-    EXPECT_FLOAT_EQ(this->box.max(i), Scalar(0));
+  this->box_.Fit(p.data());
+  for (std::size_t i = 0; i < this->box_.size(); ++i) {
+    EXPECT_EQ(this->box_.min(i), Scalar(0));
+    EXPECT_EQ(this->box_.max(i), Scalar(0));
   }
 
   // Fit a box.
   std::vector<Scalar> min(static_cast<std::size_t>(dim), Scalar(-1));
   std::vector<Scalar> max(static_cast<std::size_t>(dim), Scalar(+1));
-  this->box.Fit(BoxMap<Scalar, kDynamicDim>(min.data(), max.data(), dim));
-  for (std::size_t i = 0; i < this->box.size(); ++i) {
-    EXPECT_FLOAT_EQ(this->box.min(i), min[i]);
-    EXPECT_FLOAT_EQ(this->box.max(i), max[i]);
+  this->box_.Fit(BoxMap<Scalar, kDynamicDim>(min.data(), max.data(), dim));
+  for (std::size_t i = 0; i < this->box_.size(); ++i) {
+    EXPECT_EQ(this->box_.min(i), min[i]);
+    EXPECT_EQ(this->box_.max(i), max[i]);
   }
 }
