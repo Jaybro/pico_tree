4c7c11faa3cbb7cf206e5ad847349a0524bf3ca6
Jonathan Broere <j_broere@yahoo.com>
2021-08-15 12:19:03 +0200

    Benchmark alignment issue documentation.

--- examples/benchmark/benchmark.hpp
+++ examples/benchmark/benchmark.hpp
@@ -4,6 +4,18 @@
 
 #include "format_bin.hpp"
 
+// It seems that there is a "threshold" to the amount of functions being
+// benchmarked. Having "too many" of them makes them become slow(er). This
+// appears to be due to code alignment. See similar issue and video referencing
+// that issue:
+//
+// * https://github.com/google/benchmark/issues/461
+// * https://www.youtube.com/watch?v=10MQW-aJU3g&t=197s
+//
+// Having split up many of the benchmarks into different executables solved it
+// for a while, but the problem came back for bm_pico_kd_tree. For now,
+// benchmarks are just enabled 1-by-1.
+
 namespace pico_tree {
 
 class Benchmark : public benchmark::Fixture {
