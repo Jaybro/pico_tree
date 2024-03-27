68ecf5364c0379b74fe2a0151b595a408937188b
Jonathan Broere <jea.broere@gmail.com>
2024-03-27 21:28:11 +0100

    Split-up RunDataset.

--- examples/kd_forest/kd_forest.cpp
+++ examples/kd_forest/kd_forest.cpp
@@ -9,25 +9,46 @@
 #include "mnist.hpp"
 #include "sift.hpp"
 
-// A KdForest takes roughly forest_size times longer to build compared to
-// building a KdTree. However, the KdForest is usually a lot faster with queries
-// in high dimensions with the added trade-off that the exact nearest neighbor
-// may not be found.
-template <typename Dataset>
-void RunDataset(
-    std::size_t tree_max_leaf_size,
+template <typename Vector_, typename Scalar_>
+std::size_t RunKdForest(
+    std::vector<Vector_> const& train,
+    std::vector<Vector_> const& test,
+    std::vector<pico_tree::Neighbor<int, Scalar_>> const& nns,
     std::size_t forest_size,
     std::size_t forest_max_leaf_size,
     std::size_t forest_max_leaves_visited) {
-  using Point = typename Dataset::PointType;
-  using Space = std::reference_wrapper<std::vector<Point>>;
-  using Scalar = typename Point::value_type;
+  using Space = std::reference_wrapper<std::vector<Vector_> const>;
 
-  auto train = Dataset::ReadTrain();
-  auto test = Dataset::ReadTest();
-  std::size_t count = test.size();
-  std::vector<pico_tree::Neighbor<int, Scalar>> nns(count);
-  std::string fn_nns_gt = Dataset::kDatasetName + "_nns_gt.bin";
+  auto rkd_tree = [&train, &forest_max_leaf_size, &forest_size]() {
+    ScopedTimer t0("kd_forest build");
+    return pico_tree::KdForest<Space>(train, forest_max_leaf_size, forest_size);
+  }();
+
+  ScopedTimer t1("kd_forest query");
+  pico_tree::Neighbor<int, Scalar_> nn;
+
+  std::size_t equal = 0;
+  for (std::size_t i = 0; i < nns.size(); ++i) {
+    rkd_tree.SearchNn(test[i], forest_max_leaves_visited, nn);
+
+    if (nns[i].index == nn.index) {
+      ++equal;
+    }
+  }
+
+  return equal;
+}
+
+template <typename Vector_, typename Scalar_>
+void RunKdTree(
+    std::vector<Vector_> const& train,
+    std::vector<Vector_> const& test,
+    std::string const& fn_nns_gt,
+    std::size_t tree_max_leaf_size,
+    std::vector<pico_tree::Neighbor<int, Scalar_>>& nns) {
+  using Space = std::reference_wrapper<std::vector<Vector_> const>;
+
+  nns.resize(test.size());
 
   if (!std::filesystem::exists(fn_nns_gt)) {
     std::cout << "Creating " << fn_nns_gt
@@ -51,28 +72,38 @@ void RunDataset(
     std::cout << "KdTree not created. Read " << fn_nns_gt << " instead."
               << std::endl;
   }
+}
 
-  std::size_t equal = 0;
-  {
-    auto rkd_tree = [&train, &forest_max_leaf_size, &forest_size]() {
-      ScopedTimer t0("kd_forest build");
-      return pico_tree::KdForest<Space>(
-          train, forest_max_leaf_size, forest_size);
-    }();
+// A KdForest takes roughly forest_size times longer to build compared to
+// building a KdTree. However, the KdForest is usually a lot faster with queries
+// in high dimensions with the added trade-off that the exact nearest neighbor
+// may not be found.
+template <typename Dataset>
+void RunDataset(
+    std::size_t tree_max_leaf_size,
+    std::size_t forest_size,
+    std::size_t forest_max_leaf_size,
+    std::size_t forest_max_leaves_visited) {
+  using Point = typename Dataset::PointType;
+  using Scalar = typename Point::value_type;
 
-    ScopedTimer t1("kd_forest query");
-    pico_tree::Neighbor<int, Scalar> nn;
-    for (std::size_t i = 0; i < nns.size(); ++i) {
-      rkd_tree.SearchNn(test[i], forest_max_leaves_visited, nn);
+  auto train = Dataset::ReadTrain();
+  auto test = Dataset::ReadTest();
+  std::vector<pico_tree::Neighbor<int, Scalar>> nns;
+  std::string fn_nns_gt = Dataset::kDatasetName + "_nns_gt.bin";
 
-      if (nns[i].index == nn.index) {
-        ++equal;
-      }
-    }
-  }
+  RunKdTree(train, test, fn_nns_gt, tree_max_leaf_size, nns);
+
+  std::size_t equal = RunKdForest(
+      train,
+      test,
+      nns,
+      forest_size,
+      forest_max_leaf_size,
+      forest_max_leaves_visited);
 
   std::cout << "Precision: "
-            << (static_cast<float>(equal) / static_cast<float>(count))
+            << (static_cast<float>(equal) / static_cast<float>(nns.size()))
             << std::endl;
 }
 
